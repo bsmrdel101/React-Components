6b80a28b615ca023720b72453cd1f2f75c436e92
Bennett Smrdel <fireknights318@gmail.com>
2023-11-11 07:41:54 -0600

    feat: Create dynamic class attributes

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,11 +3,25 @@ import React from "react";
 
 interface Props {
   children: any
+  className?: string
+  noHover?: boolean
 }
 
-export default function Button({ children }: Props) {
+
+export default function Button({ children, className, noHover }: Props) {
+  const getClassNames = (): string[] => {
+    const classes = [];
+    if (noHover) classes.push('no-hover');
+    return classes;
+  };
+
+  const classNames = getClassNames();
+  const classValue = classNames.length > 0 ? `button ${classNames.join(' ')}` : 'button';
+
+
   return (
     <button
+      className={classValue}
     >
       { children }
     </button>
